8f7b0e264dadaa60310f24b4b07857236c68240e
Davi Oliveira <daviolie2@gmail.com>
2021-08-09 17:40:49 -0300

    Tipos de variáveis

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+//Console.log("Trabalhando com variáveis")
+console.log("Trabalhando com variáveis")
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Davi";
+
+console.log(idade)
+
+//não fazer
+//mes ="Janeiro"
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 20;
+let nome = "Davi";
+const sobrenome = "Oliveira";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome,sobrenome)
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto)
