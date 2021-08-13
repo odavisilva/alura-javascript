e7eaaf362fb8b6729f29d7d4164d84b127907687
Davi Oliveira <daviolie2@gmail.com>
2021-08-12 23:59:05 -0300

    Trabalhando com muitos elementos

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,11 +1,18 @@
 console.log("Trabalhando com atribuição de variáveis");
-const idade = 20;
-let nome = "Davi";
+let primeiroNome = "Davi";
 const sobrenome = "Oliveira";
 
 //console.log(nome + " " + sobrenome);
-console.log(nome,sobrenome)
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log(primeiroNome,sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
-const nomeCompleto = nome + sobrenome;
-console.log(nomeCompleto)
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade +1;
+console.log(idade);
